66c6bbc413096310aaa8d32213dbd0518d2f3c35
Kevin A Thomas <kevinat@wharton.upenn.edu>
2024-11-06 18:00:57 -0500

    Create control.R
    Initializes user-input variables

---
+++ code/control.R
@@ -0,0 +1,11 @@
+# Set user email
+my_email <- 'kevinat@wharton.upenn.edu'
+
+# Set work entity ID(s)
+
+
+# Set directories
+## Data
+data_dir <- '~/Documents/GitHub/Snowballer/data'
+## Code
+code_dir <- '~/Documents/GitHub/Snowballer/code'
