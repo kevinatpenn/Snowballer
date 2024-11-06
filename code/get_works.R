58c9b0753166c0c93874afcb9ad5916d89fb2270
Kevin A Thomas <kevinat@wharton.upenn.edu>
2024-11-06 18:01:54 -0500

    Update get_works.R
    Debug code checks paged result counts

--- code/get_works.R
+++ code/get_works.R
@@ -12,36 +12,40 @@ my_email <- 'kevinat@wharton.upenn.edu' # hard-coded example
 # Get works cited/citing
 oal_domain <- 'https://api.openalex.org/'
 fields_to_return <- c('id,doi,title,publication_year,language,type,is_retracted') # hard-coded example
+### For testing
+pulls <- c()
 ## For each work entity ID
 for(sid in seed_ids){
-  ## Initialize results storage (to file)
+  # Initialize results storage (to file)
   write.table(as.data.frame(matrix(nrow = 0,
                                    ncol = length(strsplit(fields_to_return, ",")[[1]]),
                                    dimnames = list(NULL, strsplit(fields_to_return, ",")[[1]]))), 
               file = paste0(sid, '.txt'),
               sep = '|',
               row.names = FALSE)
-  ## Get cited_by works then cites works
+  # Get cited_by works then cites works
   for(cit in c('cited_by', 'cites')){
-    ## Get result count
+    # Get result count
     pgraw <- GET(paste0(oal_domain, 'works?mailto=', my_email, 'per-page=1&page=1&select=id&filter=', cit, ':', sid))
     Sys.sleep(.1) # Obey public API rate limit of max 10 requests per second
     pgdat <- fromJSON(rawToChar(pgraw$content))
     cit_count <- pgdat$meta$count
-    ## If there are citations
+    # If there are citations
     if(cit_count > 0){
-      ## Get all results
+      # Get all results
       pg <- 0
       ppg <- 200
-      ## While there are results remaining
+      # While there are results remaining
       while((pg * ppg) < cit_count){
-        ## Stay at or below 200 results per page
+        # Stay at or below 200 results per page
         pg <- pg + 1
-        ## Get one page of results
+        # Get one page of results
         pgraw <- GET(paste0(oal_domain, 'works?mailto=', my_email, 'per-page=', ppg, '&page=', pg, '&select=', fields_to_return, '&filter=', cit, ':', sid))
         Sys.sleep(.1) # Obey public API rate limit of max 10 requests per second
         pgdat <- fromJSON(rawToChar(pgraw$content))$results
-        ## Append latest page of results to the results file
+        ### For testing
+        pulls <- c(pulls, paste('type:', cit, 'pg:', pg, 'results', nrow(pgdat)))
+        # Append latest page of results to the results file
         write.table(pgdat, 
                     file = paste0(sid, '.txt'),
                     append = TRUE,
@@ -53,5 +57,5 @@ for(sid in seed_ids){
   }
 }
 
-## Clean up temporary objects
+# Clean up temporary objects
 rm('cit', 'cit_count', 'fields_to_return', 'my_email', 'oal_domain', 'pg', 'pgdat', 'pgraw', 'ppg', 'seed_ids', 'sid')
