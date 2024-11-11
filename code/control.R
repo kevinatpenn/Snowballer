e315683a6ad986f536d4cc32ea98078b5ccabd5d
Kevin A Thomas <kevinat@wharton.upenn.edu>
2024-11-11 10:44:09 -0500

    Update control.R
    Add sequential script call logic

--- code/control.R
+++ code/control.R
@@ -1,11 +1,17 @@
 # Set user email
 my_email <- 'kevinat@wharton.upenn.edu'
 
-# Set work entity ID(s)
-
-
 # Set directories
 ## Data
 data_dir <- '~/GitHub/Snowballer/data/'
 ## Code
 code_dir <- '~/GitHub/Snowballer/code/'
+
+# Set seed work entity ID(s)
+seed_ids <- c('W3125944002') # hard-coded example
+
+# Get works
+source(paste0(code_dir, 'get_works.R'))
+
+# De-duplicate results
+source(paste0(code_dir, 'dedup_works.R'))
