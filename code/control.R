da2e29d9f7660146cf7101b0629e75a018f5ff14
Kevin A Thomas <kevinat@wharton.upenn.edu>
2024-11-12 11:49:19 -0500

    Update control.R
    Added code for dynamic degrees of separation

--- code/control.R
+++ code/control.R
@@ -3,12 +3,23 @@ my_email <- 'kevinat@wharton.upenn.edu'
 
 # Set directories
 ## Data
-data_dir <- '~/GitHub/Snowballer/data/'
+data_dir <- '~/Documents/GitHub/Snowballer/data/'
 ## Code
-code_dir <- '~/GitHub/Snowballer/code/'
+code_dir <- '~/Documents/GitHub/Snowballer/code/'
+
+# Set degrees of separation
+## Cited by
+cited_by <- 3
+## Cites
+cites <- 1
 
 # Set seed work entity ID(s)
-seed_ids <- c('W3125944002') # hard-coded example
+seed_ids <- c('W3125944002', 'W2582743722') # hard-coded example
+
+########################
+
+# Encode degrees of separation
+source(paste0(code_dir, 'degrees_separation.R'))
 
 # Get works
 source(paste0(code_dir, 'get_works.R'))
