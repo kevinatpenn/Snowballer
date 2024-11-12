9354d7f26d1224b1a524c606093e7b7ad0c0b4e0
Kevin A Thomas <kevinat@wharton.upenn.edu>
2024-11-12 16:04:24 -0500

    Update get_works.R
    Added logic to separate cites and cited_by results, storing each set of IDs for another iteration
    Stored working files in sub-directory

--- code/get_works.R
+++ code/get_works.R
@@ -2,19 +2,27 @@
 library(httr)
 library(jsonlite)
 
-# Get works cited/citing
+# Initialize
+## Open Alex API address
 oal_domain <- 'https://api.openalex.org/'
-## For each work entity ID
-for(sid in seed_ids){
-  # Initialize results storage (to file)
-  write.table(as.data.frame(matrix(nrow = 0,
-                                   ncol = length(strsplit(fields_to_return, ",")[[1]]),
-                                   dimnames = list(NULL, strsplit(fields_to_return, ",")[[1]]))), 
-              file = paste0(data_dir, sid, '.txt'),
-              sep = '|',
-              row.names = FALSE)
-  # Get current degree of separation's works cited_by and/or cites
-  for(cit in cite_degrees[[i]]){
+## Work entity ID storage for next degree of separation
+next_ids <- data.frame(cit = c(),
+                       id = c())
+## Working file directory
+dir.create(paste0(data_dir, 'working/'), showWarnings = FALSE)
+
+# Get works cited/citing
+## For current degree of separation's works cited_by and/or cites
+for(cit in cite_degrees[[deg]]){
+  ## For each work entity ID
+  for(sid in seed_ids$id[seed_ids$cit == 'seed' | seed_ids$cit == cit]){
+    # Initialize results storage (to file)
+    write.table(as.data.frame(matrix(nrow = 0,
+                                     ncol = length(strsplit(fields_to_return, ",")[[1]]),
+                                     dimnames = list(NULL, strsplit(fields_to_return, ",")[[1]]))), 
+                file = paste0(data_dir, 'working/', sid, '_', cit, '.txt'),
+                sep = '|',
+                row.names = FALSE)
     # Get result count
     pgraw <- GET(paste0(oal_domain, 'works?mailto=', my_email, '&per-page=1&page=1&select=id&filter=', cit, ':', sid))
     pgdat <- fromJSON(rawToChar(pgraw$content))
@@ -32,17 +40,26 @@ for(sid in seed_ids){
         pgdat <- fromJSON(rawToChar(pgraw$content))
         cursor <- pgdat$meta$next_cursor
         # Append latest page of results to the results file
-        write.table(pgdat$results, 
-                    file = paste0(data_dir, sid, '.txt'),
-                    append = TRUE,
-                    sep = '|',
-                    row.names = FALSE,
-                    col.names = FALSE)
+        if(!is.null(nrow(pgdat$results))){
+          write.table(pgdat$results, 
+                      file = paste0(data_dir, 'working/', sid, '_', cit, '.txt'),
+                      append = TRUE,
+                      sep = '|',
+                      row.names = FALSE,
+                      col.names = FALSE)
+          next_ids <- rbind(next_ids,
+                            data.frame(cit = rep(cit, nrow(pgdat$results)),
+                                       id = c(pgdat$results$id)))
+        }
       }
     }
   }
 }
 
+# Prepare for next iteration
+seed_ids <- next_ids
+## Clean IDs (of full path)
+
 # Clean up temporary objects
-rm('cit', 'cit_count', 'cursor', 'fields_to_return', 'oal_domain', 'pgdat', 'pgraw', 'ppg', 'seed_ids', 'sid')
+rm('cit', 'cit_count', 'cursor', 'fields_to_return', 'next_ids', 'oal_domain', 'pgdat', 'pgraw', 'ppg', 'sid')
 gc()
