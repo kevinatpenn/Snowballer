56089cc8f264e02bc540ad739c9bdf8eab26c270
Kevin A Thomas <kevinat@wharton.upenn.edu>
2025-03-06 10:13:31 -0500

    Update read_seeds.py
    Ingest first column only. Improved labels.

--- code/read_seeds.py
+++ code/read_seeds.py
@@ -2,10 +2,11 @@
 """
 Created on Wed Mar  5 17:50:02 2025
 @author: kevinatpenn
-Purpose: Read in seed work IDs
+Purpose: Ingest seed work IDs
 """
 
-seed_ids = pd.read_csv(os.path.join(data_dir, seed_file), sep=',', header=None)
+# Read first column of seed_file
+seed_ids = pd.read_csv(os.path.join(data_dir, seed_file), sep=',', header=None, usecols=[0])
 
 # Clean work ID values
 #Add/remove URL string
