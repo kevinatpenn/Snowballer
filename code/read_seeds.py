6a1398a684b960f48f5fa3815c2ad9cc3a2dba45
Kevin A Thomas <kevinat@wharton.upenn.edu>
2025-03-06 17:29:16 -0500

    Update read_seeds.py
    Implemented cleaning steps for ID lists: Remove web address; Filter for work ID format; Remove duplicates

--- code/read_seeds.py
+++ code/read_seeds.py
@@ -5,9 +5,14 @@ Created on Wed Mar  5 17:50:02 2025
 Purpose: Ingest seed work IDs
 """
 
+# Load dependencies
+import re
+
 # Read first column of seed_file
 seed_ids = pd.read_csv(os.path.join(data_dir, seed_file), sep=',', header=None, usecols=[0])
 
 # Clean work ID values
-#Add/remove URL string
-#Remove entries that aren't IDs
+seed_ids = seed_ids[0].tolist()
+seed_ids = [s.replace('https://openalex.org/', '') for s in seed_ids] # Remove web address
+seed_ids = [x for x in seed_ids if re.search(r'^[Ww]\d*', x)] # Filter for work ID format
+seed_ids = list(set(seed_ids)) # Remove duplicates
